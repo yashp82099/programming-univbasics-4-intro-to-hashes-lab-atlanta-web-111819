2ca026ead1fe68812621ac0b109399c61e46f00c
yash patel <yash82099@yahoo.com>
2019-10-11 21:39:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator[key] = value
+  return my_hash_creator
+  
 end
 
 def read_from_hash(hash, key)
