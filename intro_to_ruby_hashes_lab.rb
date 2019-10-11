50f5ba50e1e61827af8bfbad844ebfa2c9e24d1e
yash patel <yash82099@yahoo.com>
2019-10-11 21:41:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  
   my_hash_creator[key] = value
   return my_hash_creator[key] = value
   
