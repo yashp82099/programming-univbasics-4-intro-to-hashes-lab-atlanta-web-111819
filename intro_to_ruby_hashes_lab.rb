ef0bb89b185980c721e08b42ee7b291cc1f5e062
yash patel <yash82099@yahoo.com>
2019-10-11 21:34:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,8 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "Grace Hopper"}
+  return pioneer
 end
 
 def id_generator
